9a1d5208b13413981cd78658a84df174b23372f3
dipu <dipu.j247@gmail.com>
2020-05-03 23:05:51 +0600

    third commit

--- day_one.py
+++ day_one.py
@@ -5,12 +5,25 @@ number2 = 123.2312
 bool_one = True
 bool_two = False
 
+string = 'this is string example'
+string2 = "this is also a string example"
+name = "my name"
+
 print(number)
 print(number2)
 print(bool_one)
 print(bool_two)
+print(string)
+print(string2)
 
 print(type(number))
 print(type(number2))
 print(type(bool_one))
 print(type(bool_two))
+print(type(string))
+print(type(string2))
+
+def yourName():
+    print(name)
+
+yourName()
