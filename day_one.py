725041b6c4f5b8270e64e928a51b72abd450efcf
dipu <dipu.j247@gmail.com>
2020-05-03 22:59:19 +0600

    numbers example done by python

--- day_one.py
+++ day_one.py
@@ -1,1 +1,16 @@
-print("First Program in python")
+name = ''
+number = 200000000000000000
+number2 = 123.2312
+
+bool_one = True
+bool_two = False
+
+print(number)
+print(number2)
+print(bool_one)
+print(bool_two)
+
+print(type(number))
+print(type(number2))
+print(type(bool_one))
+print(type(bool_two))
